bd853090bda06f837aecc19a1a1cefc98207a990
Takoi <fvdelows@mail.ru>
2019-10-15 21:56:28 +0300

    Загрузил проект

---
+++ flask_app.py
@@ -0,0 +1,21 @@
+from flask import Flask, url_for, session, redirect, render_template, request, jsonify, \
+    make_response, \
+    request
+
+app = Flask(__name__)
+app.secret_key = 'any random string'
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/popcorn')
+def popcorn():
+    return render_template('popcorn.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
